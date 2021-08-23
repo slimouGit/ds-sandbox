739e335d99b7b9ecc423c15dd8ed3eee7cfa8fb2
Salim Oussayfi <box@slimou.net>
2021-08-23 15:32:12 +0200

    Clustering

---
+++ DS_Sandbox/012_Clustering/Exercise_01_CLU.py
@@ -0,0 +1,30 @@
+import plotly.plotly as py
+import pandas as pd
+
+df = pd.read_csv("edlich-kmeans-A0.csv")
+df.head()
+
+scatter = dict(
+    mode = "markers",
+    name = "y",
+    type = "scatter3d",
+    x = df['x'], y = df['y'], z = df['z'],
+    marker = dict( size=2, color="rgb(23, 190, 207)" )
+)
+clusters = dict(
+    alphahull = 7,
+    name = "y",
+    opacity = 0.1,
+    type = "mesh3d",
+    x = df['x'], y = df['y'], z = df['z']
+)
+layout = dict(
+    title = '3d point clustering',
+    scene = dict(
+        xaxis = dict( zeroline=False ),
+        yaxis = dict( zeroline=False ),
+        zaxis = dict( zeroline=False ),
+    )
+)
+fig = dict( data=[scatter, clusters], layout=layout )
+# Use py.iplot() for IPython notebook
